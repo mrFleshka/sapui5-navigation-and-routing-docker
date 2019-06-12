93e42640eaee1de75de0481aaa42f708e871a4ff
mrFleshka <mrFleshka@gmail.com>
2019-06-12 22:13:16 +0300

    Step 2: Enable Routing

--- webapp/Component.js
+++ webapp/Component.js
@@ -5,6 +5,14 @@ sap.ui.define([
     return UIComponent.extend("sap.ui.demo.nav.Component", {
         metadata: {
             manifest: "json"
+        },
+
+        init: function () {
+            // call the init function of the parent
+            UIComponent.prototype.init.apply(this, arguments);
+
+            // create the views based on the url/hash
+            this.getRouter().initialize();
         }
     });
 });
