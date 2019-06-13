fb15a52ec90de0cac60675394b1eef0506b34254
mrFleshka <mrFleshka@gmail.com>
2019-06-13 15:21:45 +0300

    Step 14: Make Dialogs Bookmarkable

--- webapp/controller/employee/overview/EmployeeOverviewContent.controller.js
+++ webapp/controller/employee/overview/EmployeeOverviewContent.controller.js
@@ -43,11 +43,17 @@ sap.ui.define([
                 this._applySearchFilter(this._oRouterArgs.query.search);
                 // sorting via URL hash
                 this._applySorter(this._oRouterArgs.query.sortField, this._oRouterArgs.query.sortDescending);
+                // show dialog via URL hash
+                if (!!this._oRouterArgs.query.showDialog) {
+                    this._oVSD.open();
+                }
             }
         },
 
         onSortButtonPressed : function () {
-            this._oVSD.open();
+            var oRouter = this.getRouter();
+            this._oRouterArgs.query.showDialog = 1;
+            oRouter.navTo("employeeOverview",this._oRouterArgs);
         },
 
         onSearchEmployeesTable : function (oEvent) {
@@ -64,6 +70,11 @@ sap.ui.define([
                     var oSortItem = oEvent.getParameter("sortItem");
                     this._oRouterArgs.query.sortField = oSortItem.getKey();
                     this._oRouterArgs.query.sortDescending = oEvent.getParameter("sortDescending");
+                    delete this._oRouterArgs.query.showDialog;
+                    oRouter.navTo("employeeOverview",this._oRouterArgs, true /*without history*/);
+                }.bind(this),
+                cancel : function (oEvent){
+                    delete this._oRouterArgs.query.showDialog;
                     oRouter.navTo("employeeOverview",this._oRouterArgs, true /*without history*/);
                 }.bind(this)
             });
